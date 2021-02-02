bd653b1ca3734d3f1a4d47d74391c55390028f3a
Tong LI <tl10@sanger.ac.uk>
2021-02-02 16:45:32 +0000

    separated spot detection and anchor channel preprocessing

--- call_peaks_in_anchor.py
+++ call_peaks_in_anchor.py
@@ -11,67 +11,35 @@ Preprocess all channels for decoding
 """
 import argparse
 import numpy as np
-from skimage.restoration import denoise_wavelet
-from skimage.morphology import white_tophat, disk
 import dask.array as da
-from dask_image import imread
 import tifffile as tf
-from apeer_ometiff_library import omexmlClass
 import trackpy as tp
+import pysnooper
 
 
-def get_processed_anchor_ch(anchor_img, chunk_size, spot_diameter):
-    anchor_img = da.transpose(anchor_img, (1, 2, 0))
-    anchor_img = anchor_img.rechunk((chunk_size, chunk_size, 1))
-
-    # Tophat enhancement
-    hat_enhenced = anchor_img.map_blocks(
-        white_tophat,
-        selem=np.expand_dims(disk(spot_diameter), -1),
-        dtype=np.uint16,
-    )
-
-    # Wavelet denoise
-    denoised = (
-        hat_enhenced.map_overlap(
-            denoise_wavelet,
-            depth=(args.overlaps, args.overlaps, 0),
-            trim=True,
-            boundary="nearest",
-            method="BayesShrink",
-            mode="soft",
-            sigma=spot_diameter,
-            rescale_sigma=True,
-            multichannel=False,
-        )
-        * 10 ** 4
-    )
-
-    return denoised.astype(np.uint16).compute()
-
-
+@pysnooper.snoop()
 def main(args):
-    # Retrieve anchor channel
-    imgs = imread.imread(args.ome_tif)
-    with tf.TiffFile(args.ome_tif) as fh:
-        metadata = omexmlClass.OMEXML(fh.ome_metadata)
-    pixels = metadata.image(0).Pixels
-    ch_names = np.array(pixels.get_channel_names())
-
-    processed_anchor = get_processed_anchor_ch(
-        imgs[ch_names == args.known_anchor], args.chunk_size,
-        args.spot_diameter
-    )
-
-    # Save
-    da.array(processed_anchor).to_zarr("anchor_image.zarr", "processed_anchor")
     # Call peaks
-    peaks = tp.locate(
-        processed_anchor,
-        diameter=args.spot_diameter,
-        percentile=args.trackpy_percentile,
-        separation=3
-    )
+    processed_anchor = da.from_zarr(args.anchor_zarr, "processed_anchor")
+    # processed_anchor = np.transpose(processed_anchor, (2, 0, 1))
+
+    if processed_anchor.shape[0] > 1:
+        tracks = tp.link_df(
+                tp.batch(np.array(processed_anchor),
+                    diameter=args.spot_diameter,
+                    percentile=args.trackpy_percentile,
+                    separation=args.peak_separation
+                    ),
+                search_range=5
+            )
+        # peaks = tracks['particle'][tracks['frame'] == 0].unique()
+    else:
+        peaks = tp.locate(
+            np.array(processed_anchor),
+            diameter=args.spot_diameter,
+            percentile=args.trackpy_percentile,
+            separation=args.peak_separation
+        )
 
     peaks.to_csv("anchor_peaks.tsv", sep="\t", index=False)
 
@@ -79,13 +47,12 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-ome_tif", type=str, required=True)
+    parser.add_argument("-anchor_zarr", type=str, required=True)
     parser.add_argument("-spot_diameter", type=int, default=5)
     parser.add_argument("-trackpy_percentile", type=int, default=64)
-    parser.add_argument("-known_anchor", type=str, default="c01 anchor")
-    # parser.add_argument("-whitehat_disk_diam", type=int, default=5)
-    parser.add_argument("-overlaps", type=int, default=50)
-    parser.add_argument("-chunk_size", type=int, default=4000)
+    parser.add_argument("-whitehat_disk_diam", type=int, default=5)
+    parser.add_argument("-trackpy_search_range", type=int, default=9)
+    parser.add_argument("-peak_separation", type=int, default=5)
 
     args = parser.parse_args()
 
