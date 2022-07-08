d9370f4a8457dd025b6208ae0cb70cb7c06af729
Tong LI <tl10@sanger.ac.uk>
2022-07-08 16:23:23 +0000

    batch process spot profile if the N is too large

--- bin/decode.py
+++ bin/decode.py
@@ -17,31 +17,56 @@ import pickle
 import fire
 
 
-def main(stem, spot_profile, spot_loc, barcodes_01, gene_names, channels_info):
+def main(
+    stem,
+    spot_profile,
+    spot_loc,
+    barcodes_01,
+    gene_names,
+    channels_info,
+    chunk_size=3 * 10**6,
+):
     # load
     spot_profile = np.load(spot_profile)
-    print(spot_profile)
     spot_loc = pd.read_csv(spot_loc, index_col=0)
+    print(spot_profile.shape, spot_loc)
     barcodes_01 = np.load(barcodes_01, allow_pickle=True)
     gene_names = np.load(gene_names, allow_pickle=True)
     with open(channels_info, "rb") as fp:
         channels_info = pickle.load(fp)
-
-    # print(spot_profile.shape, spot_loc)
-    # estimate GMM parameters and compute class probabilities
-    out = decoding_function(spot_profile, barcodes_01, print_training_progress=True)
-    # creating a data frame from the decoding output
-    df_class_names = np.concatenate((gene_names, ["infeasible", "background", "nan"]))
     df_class_codes = np.concatenate(
         (channels_info["barcodes_AGCT"], ["NA", "0000", "NA"])
     )
-    decoded_spots_df = decoding_output_to_dataframe(out, df_class_names, df_class_codes)
-    decoded_df = pd.concat([decoded_spots_df, spot_loc], axis=1)
+    df_class_names = np.concatenate((gene_names, ["infeasible", "background", "nan"]))
+    print(df_class_names)
 
-    decoded_df.to_csv(f"{stem}_decoded_df.tsv", sep="\t", index=False)
+    n_spot = spot_profile.shape[0]
+    n_chunk = int(np.round(n_spot / chunk_size)) + 1
 
-    with open(f"{stem}_decode_out_parameters.pickle", "wb") as fp:
-        pickle.dump(out, fp, protocol=4)
+    if n_spot >= chunk_size:
+        # if there are too much spots
+        decoded_list = [
+            decoding_function(chunk, barcodes_01, print_training_progress=False)
+            for chunk in np.array_split(spot_profile, n_chunk, axis=0)
+        ]
+        decoded_spots_df = pd.concat([decoding_output_to_dataframe(decoded, df_class_names, df_class_codes) for decoded in decoded_list])
+    else:
+        # estimate GMM parameters and compute class probabilities
+        out = decoding_function(
+            spot_profile, barcodes_01, print_training_progress=False
+        )
+        # creating a data frame from the decoding output
+        decoded_spots_df = decoding_output_to_dataframe(out, df_class_names, df_class_codes)
+        with open(f"{stem}_decode_out_parameters.pickle", "wb") as fp:
+            pickle.dump(out, fp, protocol=4)
+
+    assert decoded_spots_df.shape[0] == spot_loc.shape[0]
+    decoded_spots_df.loc[:, "y_int"] = spot_loc.y_int.values
+    decoded_spots_df.loc[:, "x_int"] = spot_loc.x_int.values
+    # decoded_df = pd.concat([decoded_spots_df, spot_loc], axis=1)
+    # assert decoded_spots_df.isnull().values.any() # shouldn't have any nan in the df
+
+    decoded_spots_df.to_csv(f"{stem}_decoded_df.tsv", sep="\t", index=False)
 
 
 if __name__ == "__main__":
