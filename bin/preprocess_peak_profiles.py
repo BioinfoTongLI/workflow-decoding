a5bf08f1bcc103bc7c600f1269047d0e223be75c
Tong LI <tl10@sanger.ac.uk>
2022-07-08 10:02:20 +0000

    faster filtering

--- bin/preprocess_peak_profiles.py
+++ bin/preprocess_peak_profiles.py
@@ -17,17 +17,17 @@ import pandas as pd
 
 def main(profiles, spot_loc, stem):
     spot_profile = np.load(profiles)
-    # print(spot_profile)
     spot_loc = pd.read_csv(spot_loc)
     cp_profile = cp.array(spot_profile)
-    mask = [0.0 not in p.ravel() for p in cp_profile]
+    mask = (cp_profile > 0).any((1, 2)).get() # keep non-zero spots only
     filtered_profiles = spot_profile[mask]
+    filtered_spot = spot_loc[mask]
+
     np.save(
         f"{stem}_filtaered_peak_intensities.npy",
         filtered_profiles,
         allow_pickle=True,
     )
-    filtered_spot = spot_loc[mask]
     filtered_spot.to_csv(f"{stem}_filtered_peak_locs.csv")
 
 
