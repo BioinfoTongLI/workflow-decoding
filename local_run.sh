f04f719a34d9ea287a28cedfaa90614466ec252a
Tong LI <tl10@sanger.ac.uk>
2022-11-28 19:57:48 +0000

    add entry as an option for the run scripts

--- local_run.sh
+++ local_run.sh
@@ -9,12 +9,12 @@
 MOUNT_POINT='/tmp/work/'
 
 DATE_WITH_TIME=`date "+%Y%m%d%H%M"`
-TRACE_FILE="$MOUNT_POINT/${USER}_${DATE_WITH_TIME}_trace/registration_trace_${DATE_WITH_TIME}.tsv"
-TMP_NF_WORK="$MOUNT_POINT/${USER}_${DATE_WITH_TIME}_work"
+TRACE_FILE="$MOUNT_POINT/registration_trace_${DATE_WITH_TIME}.tsv"
+TMP_NF_WORK="$MOUNT_POINT/"
 
 NXF_OPTS='-Dleveldb.mmap=false' NXF_WORK=$TMP_NF_WORK nextflow -trace nextflow.executor run /lustre/scratch117/cellgen/team283/tl10/gmm-decoding/main.nf \
 	-with-trace $TRACE_FILE \
 	-params-file $1 \
-	-entry Decode \
+	-entry $2 \
 	-profile local \
 	-resume
